477029cb6f689b18dbbc0fee10a09c8994fe86a7
kim-ji-hee <dream_821@naver.com>
2020-09-04 17:58:52 +0900

    Ch9 List

--- Ch9_List/CircDblLinkedList.c
+++ Ch9_List/CircDblLinkedList.c
@@ -62,37 +62,119 @@ Dnode *search(Dlist *list, const Member *x, int compare(const Member *x, const M
 }
 
 //모든 노드의 데이터를 리스트 순서대로 출력
-void Print(const Dlist *list);
+void Print(const Dlist *list)
+{
+    if(IsEmpty(list))
+        printf("노드가 없습니다.\n");
+    else {
+        Dnode *ptr = list->head->next;
+        printf("[모두 보기]\n");
+        while(ptr != list->head)
+        {
+            PrintLnMember(&ptr->data);
+            ptr = ptr->next;    //다음 노드 선택
+        }
+    }
+}
 
 //모든 노드의 데이터를 리스트 순서와 역순으로 출력
-void PrintReverse(const Dlist *list);
+void PrintReverse(const Dlist *list)
+{
+    if(IsEmpty(list))
+        printf("노드가 없습니다.\n");
+    else {
+        Dnode *ptr = list->head->prev;
+        printf("[모두 보기]");
+        while(ptr != list->head)
+        {
+            PrintLnMember(&ptr->data);
+            ptr = ptr->prev;    //앞쪽 노드 선택
+        }
+    }
+}
 
 //선택한 노드의 다음으로 진행
-int Next(Dlist *list);
+int Next(Dlist *list)
+{
+    if(IsEmpty(list) || list->crnt->next == list->head)
+        return 0;
+    list->crnt = list->crnt->next;
+    return 1;
+}
 
 //선택한 노드의 앞쪽으로 진행
-int Prev(Dlist *list);
+int Prev(Dlist *list)
+{
+    if(IsEmpty(list) || list->crnt->prev == list->head)
+        return 0;
+    list->crnt = list->crnt->prev;
+    return 1;
+}
+
+//p가 가리키는 노드 바로 다음 노드를 삽입
+void InsertAfter(Dlist *list, Dnode *p, const Member *x)
+{
+    Dnode *ptr = AllocDNode();
+    Dnode *nxt = p->next;
+    p->next = p->next->prev = ptr;
+    SetDNode(ptr, x, p, nxt);
+    list->crnt = ptr;   //삽입한 노드를 선택
+}
 
 //p가 가리키는 노드 바로 뒤에 노드를 삽입
-void InsertFront(Dlist *list, const Member *x);
+void InsertFront(Dlist *list, const Member *x)
+{
+    InsertAfter(list, list->head, x);
+}
 
 //꼬리에 노드를 삽입
-void InsertRear(Dlist *list, const Member *x);
+void InsertRear(Dlist *list, const Member *x)
+{
+    InsertAfter(list, list->head->prev, x);
+}
 
 //p가 가리키는 노드를 삭제
-void Remove(Dlist *list, Dnode *p);
+void Remove(Dlist *list, Dnode *p)
+{
+    p->prev->next = p->next;
+    p->next->prev = p->prev;
+    list->crnt = p->prev;       //삭제한 노드의 앞쪽 노드를 선택
+    free(p);
+    if(list->crnt == list->head)
+        list->crnt = list->head->next;
+}
 
 //머리 노드를 삭제
-void RemoveFront(Dlist *list);
+void RemoveFront(Dlist *list)
+{
+    if(!IsEmpty(list))
+        Remove(list, list->head->next);
+}
 
 //꼬리 노드를 삭제
-void RemoveRear(Dlist *list);
+void RemoveRear(Dlist *list)
+{
+    if(!IsEmpty(list))
+        Remove(list, list->head->prev);
+}
 
 //선택한 노드를 삭제
-void RemoveCurrent(Dlist *list);
+void RemoveCurrent(Dlist *list)
+{
+    if(list->crnt != list->head)
+        Remove(list, list->crnt);
+}
 
 //모든 노드를 삭제
-void Clear(Dlist *list);
+void Clear(Dlist *list)
+{
+    while(!IsEmpty(list))   //텅 빌 때까지
+        RemoveFront(list);  //머리 노드를 삭제
+}
 
 //원형 이중 연결 리스트 종료
-void Terminate(Dlist *list);
+void Terminate(Dlist *list)
+{
+    Clear(list);    //모든 노드를 삭제
+    free(list->head);   //더미 노드를 삭제
+}
